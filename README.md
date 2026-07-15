9d21fb583e2254786b5033273dc5139522c7b944
partum <jarring.dev@gmail.com>
2026-07-15 14:42:25 -0600

    implemented basic form of graph

--- README.md
+++ README.md
@@ -5,13 +5,14 @@
 --[x] what should be filtered?
 [x] create a dummy filter section
 [] implement graph
--- [] scatter plot
--- [] use Average_BPM and Calories_Burned as initial values
+-- [x] scatter plot
+-- [x] use Average_BPM and Calories_Burned as initial values
 -- [] only include Weight (kg),Height (m),Max_BPM,Avg_BPM,Resting_BPM,Session_Duration (hours),Calories_Burned,Fat_Percentage,BMI
 [] implement filters
 -- [] make sure if A is selected for the x-axis is can't be selected on the y-axis
+-- [] when one value is selected, update the graph 
 [] implement summary
 [] make everything look nice
 ## What I've learned
-If vite is not recognized when you run `npm run dev` and you have cloned the repository this is because the node module folder hasn't been build yet. Run `npm install` first.
+If vite is not recognized when you run `npm run dev` and you have cloned the repository this is because the node module folder hasn't been build yet. Run `npm install` first. 
 I don't think I can work on this project on my Windows PC because I created it on my Mac.
--- src/components/Graphs.jsx
+++ src/components/Graphs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 import csvText from '../assets/gym_members_exercise_tracking_synthetic_data.csv?raw';
 
@@ -11,29 +11,83 @@ const defaultData = parsedData.map((row) => row.Calories_Burned);
 
 export default function Graphs({
   data = defaultData,
-  width = 640,
-  height = 400,
-  marginTop = 20,
-  marginRight = 20,
-  marginBottom = 20,
-  marginLeft = 20,
+  marginTop = 10,
+  marginRight = 30,
+  marginBottom = 30,
+  marginLeft = 60,
+  width = 460 - marginLeft - marginRight,
+  height = 400 - marginTop - marginBottom,
 }) {
   const values = Array.isArray(data) ? data : [];
-  const x = d3.scaleLinear([0, values.length - 1], [marginLeft, width - marginRight]);
+  //const x = d3.scaleLinear([0, values.length - 1], [marginLeft, width - marginRight]);
+  const x = d3.scaleLinear()
+    .domain([0, values.length - 1])
+    .range([marginLeft, width - marginRight]);
+
   const y = d3.scaleLinear([0, Math.max(...values, 0)], [height - marginBottom, marginTop]);
   const line = d3.line((_, i) => x(i), (d) => y(d));
 
+  data = parsedData.map((row) => ({
+    Avg_BPM: Number(row.Avg_BPM) || 0,
+    Calories_Burned: Number(row.Calories_Burned) || 0,
+  }));
+
+  let maxCalories = d3.max(data, (d) => d.Calories_Burned);
+  let minCalories = d3.min(data, (d) => d.Calories_Burned);
+  let maxBPM = d3.max(data, (d) => d.Avg_BPM);
+  let minBPM = d3.min(data, (d) => d.Avg_BPM);
+
+  const GraphTest = () => {
+    const ref = useRef()
+
+    useEffect(() => {
+      const svgElement = d3.select(ref.current)
+      // Add x-axis
+      const x = d3.scaleLinear()
+        .domain([minBPM, maxBPM])
+        .range([0, width - marginRight]);
+      svgElement.append("g")
+        .attr("transform", `translate(${marginRight}, ${height - marginBottom})`)
+        .call(d3.axisBottom(x));
+      // Add y-axis
+      const y = d3.scaleLinear()
+        .domain([minCalories, maxCalories])
+        .range([height, 0]);
+      svgElement.append("g")
+        .attr("transform", `translate(${marginRight}, -${marginRight})`)
+        .call(d3.axisLeft(y));
+      // Add dots
+      svgElement.append('g')
+        .selectAll("dot")
+        .data(data)
+        .join("circle")
+        .attr("cx", function (d) { return x(d.Avg_BPM); })
+        .attr("cy", function (d) { return y(d.Calories_Burned); })
+        .attr("r", 1.5)
+        .style("fill", "#69b3a2")
+    }, [])
+
+    return (
+      <svg style={{ border: "1px solid pink" }} width={width} height={height}
+        ref={ref}
+      />
+    )
+  }
+
   return (
     <span>
-        <p>Calories Burned Over Time</p>
-    <svg width={width} height={height}>
-      <path fill="none" stroke="currentColor" strokeWidth="1.5" d={line(values)} />
-      <g fill="white" stroke="currentColor" strokeWidth="1.5">
-        {values.map((d, i) => (
-          <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />
-        ))}
-      </g>
-    </svg>
+      <p>Calories Burned Over Time</p>
+      <svg width={width} height={height}>
+        <path fill="none" stroke="currentColor" strokeWidth="1.5" d={line(values)} />
+        <g fill="white" stroke="currentColor" strokeWidth="1.5">
+          {values.map((d, i) => (
+            <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />
+          ))}
+        </g>
+        <g transform='translate(0, ${height})'></g>
+      </svg>
+      {GraphTest()}
+      <h2>Test</h2>
     </span>
   );
 }
